614ab1095287495e9d5f33ac864b950e9ba9f954
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-02-22 12:15:41 -0800

    test: support organization-level webhooks

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -3,7 +3,7 @@ import { App } from "@octokit/app";
 
 import WebhookRelay from "../index.js";
 
-test("smoke", (t) => {
+test("repository relay", (t) => {
   t.true(WebhookRelay instanceof Function);
 
   const app = new App({
@@ -17,6 +17,28 @@ test("smoke", (t) => {
   const relay = new WebhookRelay({
     owner: "gr2m",
     repo: "github-webhooks-relay",
+    createHookToken: "token",
+    app,
+  });
+
+  t.true(relay.on instanceof Function);
+  t.true(relay.start instanceof Function);
+  t.true(relay.stop instanceof Function);
+});
+
+test("organization relay", (t) => {
+  t.true(WebhookRelay instanceof Function);
+
+  const app = new App({
+    appId: 1,
+    privateKey: "<private key>",
+    webhooks: {
+      secret: "secret123",
+    },
+  });
+
+  const relay = new WebhookRelay({
+    owner: "gr2m-sandbox",
     createHookToken: "token",
     app,
   });
